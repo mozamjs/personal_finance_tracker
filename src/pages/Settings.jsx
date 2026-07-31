4df86835df2ec17a2743d38d6309670a80fb7a29
mozamjs <mozamqurexhi38@gmail.com>
2026-08-01 00:20:00 +0500

    change

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,6 +1,6 @@
 function Settings({ darkMode, setDarkMode }) {
   return (
-    <div className="px-6 pb-10">
+    <div className="mt-5 px-6 pb-10">
       <div className="bg-white dark:bg-[#111111] border border-gray-200 dark:border-[#222222] shadow-sm dark:shadow-none rounded-xl p-5">
         <h2 className="font-semibold text-lg mb-3 text-gray-900 dark:text-white">Settings</h2>
 
